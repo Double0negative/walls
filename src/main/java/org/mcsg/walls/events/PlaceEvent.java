c440d7f196537c089bca6de3b9c8b1f780345f3b
Double0negative <Double0negative@mc-sg.org>
2013-07-14 20:38:57 -0400

    Place is blacklist, not whitelist

--- src/main/java/org/mcsg/walls/events/PlaceEvent.java
+++ src/main/java/org/mcsg/walls/events/PlaceEvent.java
@@ -1,8 +1,6 @@
 package org.mcsg.walls.events;
 
-
 import java.util.ArrayList;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,48 +10,46 @@ import org.mcsg.walls.Game;
 import org.mcsg.walls.GameManager;
 import org.mcsg.walls.SettingsManager;
 
+public class PlaceEvent
+  implements Listener
+{
+  public ArrayList<Integer> notallowedPlace = new ArrayList<Integer>();
 
+  public PlaceEvent() {
+    this.notallowedPlace.addAll(SettingsManager.getInstance().getConfig().getIntegerList("block.place.blacklist"));
+  }
+  @EventHandler(priority=EventPriority.HIGHEST)
+  public void onBlockPlace(BlockPlaceEvent event) {
+    Player p = event.getPlayer();
+    int id = GameManager.getInstance().getPlayerGameId(p);
 
-public class PlaceEvent implements Listener {
+    if (id == -1) {
+      int gameblockid = GameManager.getInstance().getBlockGameId(event.getBlock().getLocation());
+      if ((gameblockid != -1) && 
+        (GameManager.getInstance().getGame(gameblockid).getGameMode() != Game.GameMode.DISABLED)) {
+        event.setCancelled(true);
+      }
 
-    public  ArrayList<Integer> allowedPlace = new ArrayList<Integer>();
-
-    public PlaceEvent(){
-        allowedPlace.addAll( SettingsManager.getInstance().getConfig().getIntegerList("block.place.whitelist"));
+      return;
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onBlockPlace(BlockPlaceEvent event) {
-        Player p = event.getPlayer();
-        int id  = GameManager.getInstance().getPlayerGameId(p);
-
-        if(id == -1){
-            int gameblockid = GameManager.getInstance().getBlockGameId(event.getBlock().getLocation());
-            if(gameblockid != -1){
-                if(GameManager.getInstance().getGame(gameblockid).getGameMode() != Game.GameMode.DISABLED){
-                    event.setCancelled(true);
-                }
-            }
-            return;
-        }
-
-
-        Game g = GameManager.getInstance().getGame(id);
-        if(g.isPlayerinactive(p)){
-            return;
-        }
-        if(g.getMode() == Game.GameMode.DISABLED){
-            return;
-        }
-        if(g.getMode() != Game.GameMode.INGAME){
-            event.setCancelled(true);
-            return;
-
-        }
-
-        if(!allowedPlace.contains(event.getBlock().getTypeId())){
-            event.setCancelled(true);
-        }
-
+    Game g = GameManager.getInstance().getGame(id);
+    if (g.isPlayerinactive(p)) {
+      return;
     }
+    if (g.getMode() == Game.GameMode.DISABLED) {
+      return;
+    }
+    if (g.getMode() != Game.GameMode.INGAME) {
+      event.setCancelled(true);
+      return;
+    }
+
+	if(id != GameManager.getInstance().getBlockGameId(event.getBlock().getLocation())){
+		event.setCancelled(true);
+		return;
+	}
+    if (this.notallowedPlace.contains(Integer.valueOf(event.getBlock().getTypeId())))
+      event.setCancelled(true);
+  }
 }
